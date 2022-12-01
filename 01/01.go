9a2cbcba047a5c5a0b8d801959de495092bfff18
Róbert Belanec <belanecrobert22@gmail.com>
2022-12-01 09:07:10 +0100

    first day

--- 01/01.go
+++ 01/01.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func check(e error) {
@@ -11,8 +14,43 @@ func check(e error) {
 	}
 }
 
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
 func main() {
-	data, err := os.ReadFile("./input.txt")
+	readFile, err := os.Open("./input.txt")
+
 	check(err)
-	fmt.Print(string(data))
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileNums []int
+	var fileSums []int
+	// maxNums := []int{0, 0, 0}
+
+	for fileScanner.Scan() {
+		str := fileScanner.Text()
+
+		if str != "" {
+			num, err := strconv.Atoi(str)
+			check(err)
+
+			fileNums = append(fileNums, num)
+		} else {
+			sum := sum(fileNums)
+
+			fileSums = append(fileSums, sum)
+
+			fileNums = nil
+		}
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(fileSums)))
+	fmt.Printf("%v", fileSums)
+	fmt.Printf("%d", sum(fileSums[0:3]))
 }
