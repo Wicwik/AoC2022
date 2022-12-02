69480375a2d9571ddec888c44c656b94b84eb035
Róbert Belanec <belanecrobert22@gmail.com>
2022-12-02 14:37:20 +0100

    day 2

--- 02/02.go
+++ 02/02.go
@@ -23,6 +23,18 @@ func sum(array []int) int {
 func main() {
 	readFile, err := os.Open("./input.txt")
 
+	points_map := make(map[string]int)
+	wld_map := make(map[string]int)
+	points := 0
+
+	points_map["A"] = 1
+	points_map["B"] = 2
+	points_map["C"] = 3
+
+	wld_map["X"] = -1
+	wld_map["Y"] = 0
+	wld_map["Z"] = 1
+
 	check(err)
 
 	fileScanner := bufio.NewScanner(readFile)
@@ -31,6 +43,22 @@ func main() {
 	for fileScanner.Scan() {
 		str := fileScanner.Text()
 
-		fmt.Print(str)
+		to_add := points_map[str[0:1]] + wld_map[str[2:3]]
+
+		if to_add == 0 || to_add == 3 {
+			points += 3
+		} else {
+			points += (to_add % 3)
+		}
+
+		if str[2:3] == "Z" {
+			points += 6
+		}
+
+		if str[2:3] == "Y" {
+			points += 3
+		}
 	}
+
+	fmt.Println(points)
 }
