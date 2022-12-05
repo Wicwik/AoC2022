e59e99343ad46271f851e27d36719b5b8c5b9c25
Róbert Belanec <belanecrobert22@gmail.com>
2022-12-05 17:14:12 +0100

    prepared for all days

---
+++ 22/22_b.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+func main() {
+	readFile, err := os.Open("./input.txt")
+
+	check(err)
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		str := fileScanner.Text()
+
+		fmt.Print(str)
+	}
+}
