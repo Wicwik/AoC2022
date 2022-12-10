5223539841301eb19cb3f70bf6aa588c2fba2bf0
Róbert Belanec <belanecrobert22@gmail.com>
2022-12-10 11:02:21 +0100

    day 9

--- 09/09_a.go
+++ 09/09_a.go
@@ -3,7 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/emirpasic/gods/sets/hashset"
 )
 
 func check(e error) {
@@ -20,17 +25,86 @@ func sum(array []int) int {
 	return result
 }
 
+type Pair struct {
+	a, b int
+}
+
+type dirPair struct {
+	a Pair
+	b int
+}
+
+func follow_head(head Pair, tail Pair) Pair {
+	x, y := head.a-tail.a, head.b-tail.b
+	abx, aby := int(math.Abs(float64(x))), int(math.Abs(float64(y)))
+
+	var newx int
+	var newy int
+	if abx > 1 || aby > 1 {
+		if x == 0 {
+			newx = tail.a
+		} else {
+			newx = tail.a + (x / abx)
+		}
+
+		if y == 0 {
+			newy = tail.b
+		} else {
+			newy = tail.b + (y / aby)
+		}
+
+		return Pair{newx, newy}
+	}
+
+	return tail
+}
+
 func main() {
+	directions := map[string]Pair{
+		"L": {-1, 0},
+		"R": {1, 0},
+		"U": {0, 1},
+		"D": {0, -1},
+	}
+
 	readFile, err := os.Open("./input.txt")
+
+	knots := 10
 
 	check(err)
 
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
+	var moves []dirPair
+
 	for fileScanner.Scan() {
 		str := fileScanner.Text()
+		str_split := strings.Split(str, " ")
+		steps, err := strconv.Atoi(str_split[1])
+		check(err)
 
-		fmt.Print(str)
+		moves = append(moves, dirPair{directions[str_split[0]], steps})
 	}
+
+	var rope []Pair
+	for i := 0; i < knots; i++ {
+		rope = append(rope, Pair{0, 0})
+	}
+
+	visited := hashset.New()
+	visited.Add(rope[len(rope)-1])
+
+	for _, move := range moves {
+		for i := 0; i < int(move.b); i++ {
+			head := rope[0]
+			rope[0] = Pair{head.a + move.a.a, head.b + move.a.b}
+			for j := 1; j < knots; j++ {
+				rope[j] = follow_head(rope[j-1], rope[j])
+			}
+			visited.Add(rope[len(rope)-1])
+		}
+	}
+
+	fmt.Println(visited.Size())
 }
---
+++ 09/go.mod
@@ -0,0 +1,5 @@
+module github.com/Wicwik/AoC2022/08
+
+go 1.19
+
+require github.com/emirpasic/gods v1.18.1
