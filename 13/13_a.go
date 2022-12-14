2ccda8c05fa0452edeb5442e30fdef2a39e11fac
Róbert Belanec <belanecrobert22@gmail.com>
2022-12-14 21:28:17 +0100

    day 13

--- 13/13_a.go
+++ 13/13_a.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -20,17 +21,51 @@ func sum(array []int) int {
 	return result
 }
 
+func compare(left, right any) int {
+	lefts, leftok := left.([]any)
+	rights, rightok := right.([]any)
+
+	if !leftok && !rightok {
+		return int(left.(float64) - right.(float64))
+	} else if !leftok {
+		lefts = []any{left}
+	} else if !rightok {
+		rights = []any{right}
+	}
+
+	for i := 0; i < len(lefts) && i < len(rights); i++ {
+		cmp := compare(lefts[i], rights[i])
+
+		if cmp != 0 {
+			return cmp
+		}
+	}
+
+	return len(lefts) - len(rights)
+}
+
 func main() {
-	readFile, err := os.Open("./input.txt")
+	readFile, err := os.ReadFile("./input.txt")
 
 	check(err)
 
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
+	in_right_order := 0
 
-	for fileScanner.Scan() {
-		str := fileScanner.Text()
+	for i, v := range strings.Split(string(readFile), "\n\n") {
+		str_pair := strings.Split(v, "\n")
 
-		fmt.Print(str)
+		var left, right any
+
+		err := json.Unmarshal([]byte(str_pair[0]), &left)
+		check(err)
+
+		err = json.Unmarshal([]byte(str_pair[1]), &right)
+		check(err)
+
+		if compare(left, right) <= 0 {
+			in_right_order += i + 1
+		}
 	}
+
+	fmt.Println(in_right_order)
 }
--- 13/13_b.go
+++ 13/13_b.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 func check(e error) {
@@ -20,17 +22,60 @@ func sum(array []int) int {
 	return result
 }
 
+func compare(left, right any) int {
+	lefts, leftok := left.([]any)
+	rights, rightok := right.([]any)
+
+	if !leftok && !rightok {
+		return int(left.(float64) - right.(float64))
+	} else if !leftok {
+		lefts = []any{left}
+	} else if !rightok {
+		rights = []any{right}
+	}
+
+	for i := 0; i < len(lefts) && i < len(rights); i++ {
+		cmp := compare(lefts[i], rights[i])
+
+		if cmp != 0 {
+			return cmp
+		}
+	}
+
+	return len(lefts) - len(rights)
+}
+
 func main() {
-	readFile, err := os.Open("./input.txt")
+	readFile, err := os.ReadFile("./input.txt")
 
 	check(err)
 
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
+	packets := []any{}
 
-	for fileScanner.Scan() {
-		str := fileScanner.Text()
+	for _, v := range strings.Split(string(readFile), "\n\n") {
+		str_pair := strings.Split(v, "\n")
 
-		fmt.Print(str)
+		var left, right any
+
+		err := json.Unmarshal([]byte(str_pair[0]), &left)
+		check(err)
+
+		err = json.Unmarshal([]byte(str_pair[1]), &right)
+		check(err)
+
+		packets = append(packets, left, right)
+
 	}
+
+	packets = append(packets, []any{[]any{2.}}, []any{[]any{6.}})
+	sort.Slice(packets, func(i, j int) bool { return compare(packets[i], packets[j]) < 0 })
+
+	separator_prod := 1
+	for i, p := range packets {
+		if fmt.Sprint(p) == "[[2]]" || fmt.Sprint(p) == "[[6]]" {
+			separator_prod *= i + 1
+		}
+	}
+
+	fmt.Println(separator_prod)
 }
